93afbd51b811e4352266c25f0245e789931f787b
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-18 14:20:46 +0200

    code cleanup, new helper methods

--- Dictionary.java
+++ Dictionary.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -13,17 +14,17 @@ import java.util.LinkedList;
  */
 public class Dictionary {
 	private final int HASH_MULTIPLIER;
-	private ArrayList<LinkedList<String>> wordHashes;
+	public ArrayList<LinkedList<String>> wordHashes;
 
 	public Dictionary() {
 		GeneratePrimes p = new GeneratePrimes();
 		HASH_MULTIPLIER = p.generatePrime();
-
 		setupDictionary();
 	}
 
 	/**
 	 * Generate a new dictionary with hash values.
+	 * @throws IOException 
 	 */
 	private void setupDictionary() {
 		int collisions = 0;
@@ -34,7 +35,7 @@ public class Dictionary {
 		wordHashes = new ArrayList<>();
 		
 		//Set the size array list to half the length of available words.
-		for (int i = 0; i < words.size() * 0.5; i++) {
+		for (int i = 0; i < words.size(); i++) {
 			wordHashes.add(i, null);
 		}
 		
@@ -44,32 +45,31 @@ public class Dictionary {
 		for (String word : words) {		
 			// Generate hash values and set as current index
 			int index = generateHash(word);
-
-			// Assign the words to the list at the position of hash values
-			if (hasLinkedList(wordHashes.get(index))) {
-				
-				//if collisions occur increment
-				collisions++;
-				
-				// Check if the word is already in the LinkedList
-				if (!hasWord(wordHashes.get(index), word)) {
-					int currentChainLength = 0;
-					wordHashes.get(index).add(word);
-					currentChainLength =  wordHashes.get(index).size()+1;
-					
-					//If the newly found chain is longer, update the value
-					if (currentChainLength > longestChain) {
-						longestChain = currentChainLength;
-					}
-				}
-			} else {
-				//if there's no linked list already, create a new one
-				wordHashes.add(index, new LinkedList<String>());
-				wordHashes.get(index).add(word);
+//			LinkedList<String> currentList = wordHashes.get(index);
+			
+			// Check if the word is already in the LinkedList
+            if (wordHashes.get(index) == null) {
+            	wordHashes.set(index, new LinkedList<String>());
+            } else {
+                collisions++;
+            }
+            
+            int currentChainLength = 0;
+            
+            // Assign the words to the list at the position of hash values
+            wordHashes.get(index).add(word);
+			
+            currentChainLength = wordHashes.get(index).size();
+			
+			//If the newly found chain is longer, update the value
+			if (currentChainLength > longestChain) {
+				longestChain = currentChainLength;
 			}
+
 		}
-		System.out.println("There are " + collisions + " collisions in the table.");
-		System.out.println("Longest chain is " + longestChain);
+		System.out.println("There are " + wordHashes.size() + " entries in the table.\n");
+		System.out.println("There are " + collisions + " collisions in the table.\n");
+		System.out.println("Longest chain is " + longestChain +"\n");
 	}
 	
 	//Helper Methods start here
@@ -78,35 +78,81 @@ public class Dictionary {
 	 * Helper method to generate a word's hash value.
 	 * 
 	 * @param word	Word to calculate the has for.
-	 * @return	The integer hash value.
+	 * @return		The integer hash value.
 	 */
 	private int generateHash(String word) {
-		int h = 0;
-		for (int i = 0; i < word.length(); i++) {
-			h = HASH_MULTIPLIER * h + word.charAt(i);
+		//Normalize the word
+		word = word.toUpperCase();
+		char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        
+		long hash = 0;
+		
+		for (int i = 0; i < chars.length; i++) {
+			hash = HASH_MULTIPLIER * hash + chars[i];
 		}
-		h = Math.abs(h % wordHashes.size());
-		return h;
+		
+		hash = Math.abs(hash % wordHashes.size());
+		return (int) hash;
 	}
 	
 	/**
-	 * Checks it a linked list exists in the array.
+	 * Generate a hash of any given word.
 	 * 
-	 * @param list	List to be checked
-	 * @return	True or false, if it exists
+	 * @param inputWord	The word to generate hash for.
+	 * @return	Hash value.
 	 */
-	private boolean hasLinkedList(LinkedList<String> list) {
-		return list != null;
+	private LinkedList<String> getWords(String inputWord) {
+		//Generate the word's hash value as an index
+		int hash = generateHash(inputWord);
+		
+		//Return the found list.
+		return wordHashes.get(hash);
 	}
-
+	
 	/**
-	 * Checks if a linked list has a certain word.
+	 * Find all permutations of a given word.
 	 * 
-	 * @param list	List to be checked
-	 * @param word	Word to be found
-	 * @return	True or false, if it exists
+	 * @param word	Word to be found.
 	 */
-	private boolean hasWord(LinkedList<String> list, String word) {
-		return list.contains(word);
+	public String checkPermutations(String word) {	
+		LinkedList<String> list = getWords(word);
+		
+		String perms = "";
+			
+		for (String element : list) {
+			if (isPermutation(element, word)) {
+				perms = perms + ", " + element;
+				perms = perms.substring(1, perms.length());
+			}
+		}
+		return perms;
+	}
+	
+	/**
+	 * Compare, if two words are true permutations of each other.
+	 * 
+	 * @param listWord	First word
+	 * @param inputWord	Second word
+	 * @return		Results true if they are REALLY permutable
+	 */
+	private boolean isPermutation(String listWord, String inputWord) {
+		//Normalize the word
+		listWord = listWord.toUpperCase();
+		inputWord = inputWord.toUpperCase();
+		
+		char[] chars1 = listWord.toCharArray();
+		char[] chars2 = inputWord.toCharArray();
+		
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        
+       listWord = new String(chars1);
+       inputWord = new String(chars2);
+       
+       if (listWord.equals(inputWord)) {
+    	   return true;
+       }
+       return false;
 	}
 }
